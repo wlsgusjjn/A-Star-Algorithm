d3eacefe6c8c704194f4f3eb0bc5995aefe40694
김진현 <wlsgusjjn@gmail.com>
2020-08-06 00:55:33 +0900

    A-star Pathfinding

--- A_star.cpp
+++ A_star.cpp
@@ -10,46 +10,98 @@ int maze[MAX][MAX];
 
 int mark[MAX][MAX];
 
-int distance(int x, int y) {
-	int w = 30 - x;
-	int v = 40 - y;
+int path[1000][2];
+int t_path[1000][2];
+
+int N, M, t;
+
+int sx, sy, dx, dy;
+
+void Astar(int sx, int sy, int dx, int dy);
+
+int distance(int a, int b, int x, int y) {
+	int w = a - x;
+	int v = b - y;
 	return sqrt(w * w + v * v);
 }
 
 int main() {
-	int N, M;
+	N, M;
 	cin >> N >> M;
-	for (int i = 0; i < 30; i++) {
+
+	cout << "Source: ";
+	cin >> sx >> sy;
+
+	cout << "Destination: ";
+	cin >> dx >> dy;
+
+
+	// 1 = wall
+	for (int i = 0; i < N; i++) {
 		string tmp;
 		cin >> tmp;
-		for (int j = 0; j < 40; j++)
+		for (int j = 0; j < M; j++) {
 			maze[i][j] = tmp[j] - '0';
+		}
 	}
 
-	int path[1000][2];
+	Astar(sx, sy, dx, dy);
 
-	int p[2] = { 1, 1 };
-	mark[1][1] = 1;
+	int j = 0;
+	for (int i = t; i > -1; i--) {
+		t_path[j][0] = path[i][0];
+		t_path[j][1] = path[i][1];
+
+		int a, b;
+		int z = -1;
+		for (int k = i - 1; k > -1; k--) {
+			a = t_path[j][0] - path[k][0];
+			b = t_path[j][1] - path[k][1];
+			if (a * a + b * b <= 2)
+				z = k;
+		}
+		i = z + 1;
+		j++;
+	}
+
+	for (int i = 0; i < j; i++)
+		maze[t_path[i][0]][t_path[i][1]] = -1;
+
+	printf("\n");
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if ((sx == i && sy == j) || (maze[i][j] == -1))	printf("* ");
+			else	printf("%d ", maze[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void Astar(int sx, int sy, int dx, int dy){
+	int p[2] = { sx, sy };
+	mark[sx][sy] = 1;
 	int n = MAX;
 	int pq[MAX][2];
 	for (int i = 0; i < n; i++) {
 		pq[i][0] = 0; pq[i][1] = 0;
 	}
+	for (int i = 0; i < 1000; i++) {
+		path[i][0] = 0; path[i][1] = 0;
+	}
+
 	int pqdis[MAX] = { 0, };
 
-	int dir[9][2] = { {-1,-1},{-1,0},{1,-1},
-						{-1,0}, {0,0}, {1,0},
-						{-1,1}, {0,1}, {1,1} };
+	int dir[9][2] = { {-1,-1},{-1,0},{-1,1},
+						{0,-1}, {0,0}, {0,1},
+						{1,-1}, {1,0}, {1,1} };
 
-	path[0][0] = 1; path[0][1] = 1;
-	int t = 0;
-
+	path[0][0] = sx; path[0][1] = sy;
+	t = 0;
 	int g = 0;
 
-	while (!(pq[0][0] == N - 2 && pq[0][1] == M - 2)) {
+	while (!(pq[0][0] == dx && pq[0][1] == dy)) {
 		g++;
 		if (!(pq[0][0] == 0 && pq[0][1] == 0)) {
-
 			p[0] = pq[0][0];
 			p[1] = pq[0][1];
 
@@ -67,7 +119,7 @@ int main() {
 				int temp[2] = { p[0] + dir[i][0] , p[1] + dir[i][1] };
 				if (maze[temp[0]][temp[1]] == 0 && mark[temp[0]][temp[1]] == 0) {
 					mark[temp[0]][temp[1]] = 1;
-					int dis = distance(temp[0], temp[1]) + g;
+					int dis = distance(dx, dy, temp[0], temp[1]) + g;
 					for (int j = 0; j < n; j++) {
 						if (temp[0] == pq[j][0] && temp[1] == pq[j][1])	break;
 						if (pqdis[j] == 0) {
@@ -92,52 +144,5 @@ int main() {
 			}
 		}
 	}
-	int c, d;
-	int tf = t;
-	int tp = t;
-	int z = 0;
-	for (int j = t; j > 1; j--) {
-		int a = path[tf][0] - path[j - 1][0];
-		int b = path[tf][1] - path[j - 1][1];
-
-		if (a * a + b * b <= 2) {
-			if (z == 1) {
-				c = path[tf][0] - path[j - 2][0];
-				d = path[tf][1] - path[j - 2][1];
-
-				if (c * c + d * d <= 2)	j--;
-
-				tp = j;
-				z = 0;
-				for (int i = tp; i < tf; i++) {
-					path[i][0] = -1; path[i][1] = -1;
-				}
-				tf = j - 1;
-				tp = j - 1;
-			}
-			else {
-				tp = j - 1;
-				tf = j - 1;
-			}
-		}
-		else {
-			z = 1;
-		}
-	}
-
-	for (int i = 0; i <= t; i++) {
-		if (!(path[i][0] == -1)) {
-			maze[path[i][0]][path[i][1]] = -1;
-		}
-	}
-
-	printf("\n");
-	p[0] = pq[0][0]; p[1] = pq[0][1];
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if ((p[0] == i && p[1] == j) || (maze[i][j] == -1))	printf("* ");
-			else	printf("%d ", maze[i][j]);
-		}
-		printf("\n");
-	}
+	path[++t][0] = pq[0][0]; path[t][1] = pq[0][1];
 }
